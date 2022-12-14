caffef117c6a4a9d43669dd30d7927b8185acc73
Ivan Kravchenko <109036277+Vano202@users.noreply.github.com>
2022-12-14 10:54:52 +0200

    new

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,141 @@
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+
+// const gallery = document.querySelector(".gallery");
+
+// const imgGallery = imgCard(galleryItems);
+// gallery.insertAdjacentElement(`beforeend`, imgGallery);
+
+// function imgCard(galleryItems) {
+//     return galleryItems
+//      .map(({original, preview, description}) => {
+//         return `<div class="gallery__item">
+//         <a class="gallery__link" href="large-image.jpg>
+//         <img
+//         class="gallery__image"
+//         src="${preview}"
+//         data-source="${original}"
+//         alt="${description}"
+//         />
+//       </a>
+//     </div>`;
+//          })
+//          .join("");
+// }
+
+// gallery.addEventListener("click", OpenClickModal);
+
+// function OpenClickModal(event){
+//     clickBlock(event);
+//     if(event.target.nodeName !== "IMG") {
+//         return;
+//     }
+//     modalBigImg();
+// }
+
+// function modalBigImg(){
+//     const instance = basicLightbox.create(
+//         `<img src="${event.target.dataset.source}">`,
+//         { Close: (instance) => {
+//             window.removeEventListener("keydown", closeEsc);
+           
+//         },
+//     }
+
+//     )
+
+//     if (instance.show(instance)) {
+//         window.addEventListener("keydown", closeEsc);
+//     }
+
+//     function closeEsc(e) {
+//         if (e.code === "Escape" && instance.visible()) {
+//             instance.close();
+          
+//         }
+//     }    
+// };
+
+// function clickBlock(event) {
+//     event.preventDefault();
+// }
+
+
+
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery");
+
+
+const imgGallery= imgCard(galleryItems);
+gallery.insertAdjacentHTML(`beforeend`, imgGallery);
+
+
+function imgCard(galleryItems) {
+    return galleryItems
+        .map(({ original, preview, description }) => {
+            return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+        })
+        .join("");
+}
+
+gallery.addEventListener("click", OpenClickModal);
+
+function OpenClickModal(event) {
+    clickBlock(event);
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    modalBigImg();
+};
+
+
+
+function modalBigImg() {
+    const instance = basicLightbox.create(
+    `
+    <img src="${event.target.dataset.source}">`
+    
+,
+    {
+        Close: (instance) => {
+            window.removeEventListener("keydown", closeEsc);
+           
+        },
+    }
+   
+    )
+    
+    if (instance.show(instance)) {
+        window.addEventListener("keydown", closeEsc);
+    }
+    
+ 
+
+    function closeEsc(e) {
+        if (e.code === "Escape" && instance.visible()) {
+            instance.close();
+          
+        }
+    }
+};
+
+
+
+function clickBlock(event) {
+    event.preventDefault();
+}
